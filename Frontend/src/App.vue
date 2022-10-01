2e75cb713b29ab19caf86b09e4e17e32eb4818ad
JY <jamesyang40k@gmail.com>
2022-10-01 15:15:20 +1300

    make empty TodoList and Footer

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import TodoHeader from './components/TodoHeader.vue';
-import TodoInput from './components/TodoInput.vue';
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
 const description = ref('')
 const items = ref([])
@@ -9,23 +11,6 @@ const items = ref([])
 onMounted(() => {
   // todo
 })
-
-async function getItems() {
-  try {
-    alert('todo')
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-
-async function handleMarkAsComplete(item) {
-  try {
-    alert('todo')
-  } catch (error) {
-    console.error(error)
-  }
-}
 </script>
 
 <template>
@@ -42,39 +27,11 @@ async function handleMarkAsComplete(item) {
       <br />
       <div class="row">
         <div class="col">
-          <h1>
-            Showing {{ items.length }} Item(s)
-            <button type="button" class="pull-right btn btn-primary" @click="getItems">Refresh</button>
-          </h1>
-          <table class="table table-striped table-bordered table-hover">
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Description</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in items" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td>{{ item.description }}</td>
-                <td>
-                  <button type="button" class="btn btn-warning btn-sm" @click="() => handleMarkAsComplete(item)">
-                    Mark as completed
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <TodoList :items="items" />
         </div>
       </div>
     </div>
-    <footer className="page-footer font-small teal pt-4">
-      <div className="footer-copyright text-center py-3">
-        © 2021 Copyright:
-        <a href="https://clearpoint.digital" target="_blank" rel="noreferrer"> clearpoint.digital </a>
-      </div>
-    </footer>
+    <TodoFooter />
   </main>
 </template>
 
---
+++ Frontend/src/components/TodoList.vue
@@ -0,0 +1,51 @@
+<script setup>
+const props = defineProps({
+  items: Array
+})
+
+
+async function getItems() {
+  try {
+    alert('todo')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+async function handleMarkAsComplete(item) {
+  try {
+    alert('todo')
+  } catch (error) {
+    console.error(error)
+  }
+}
+</script>
+
+
+<template>
+  <h1>
+    Showing {{ items.length }} Item(s)
+    <button type="button" class="pull-right btn btn-primary" @click="getItems">Refresh</button>
+  </h1>
+  <table class="table table-striped table-bordered table-hover">
+    <thead>
+    <tr>
+      <th>Id</th>
+      <th>Description</th>
+      <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="item in items" :key="item.id">
+      <td>{{ item.id }}</td>
+      <td>{{ item.description }}</td>
+      <td>
+        <button type="button" class="btn btn-warning btn-sm" @click="() => handleMarkAsComplete(item)">
+          Mark as completed
+        </button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
