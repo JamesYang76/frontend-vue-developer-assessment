27b7dacae9ec83b2b1501e653fa0e8fb57ad5620
JY <jamesyang40k@gmail.com>
2022-10-02 14:17:29 +1300

    change " to '

--- Frontend/src/api/todos.js
+++ Frontend/src/api/todos.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import {TODOS_URL} from "../constants/url";
+import axios from 'axios';
+import {TODOS_URL} from '../constants/url';
 
 export const addTodoItemAPI = (todoItem) => axios.post(TODOS_URL, todoItem)
 export const getTodoItemsAPI = () => axios.get(TODOS_URL)
